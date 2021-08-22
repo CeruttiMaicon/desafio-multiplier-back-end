598f408b7de5443efa7e4e5220b5d4e9477b5c21
alissonandrade2020 <alisson2d@hotmail.com>
2021-08-22 08:54:22 -0300

    Estruturando os Controllers

--- app/Http/Controllers/ProgressController.php
+++ app/Http/Controllers/ProgressController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class ProgressController extends Controller
 {
-    //
+    protected $statusRepository;
+
+    public function __construct(StatusRepository $statusRepository)
+    {
+        $this->statusRepository = $statusRepository;
+    }
+
+    public function list(): JsonResponse
+    {
+        $items = $this->statusRepository->list();
+
+        return response()->json(['items' => $items]);
+    }
 }
